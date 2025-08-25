cd0802854ba73d61740f8e87ae0f3eccbfec77da
fieldmarcel <shiva9590t@gmail.com>
2025-08-25 23:43:53 +0530

    server action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+import { db } from "@/db";
 
-export default function Home() {
+export default async  function Home() {
+const snippets= await db.snippet.findMany();
+
+const renderedsnippets= snippets.map((snippet)=>{
   return (
-   <div>Home page</div>
+    <div key={snippet.id} className="bg-white p-4 rounded-lg shadow-md mb-4">
+      <h3 className="text-xl font-semibold mb-2">{snippet.title}</h3>
+      <pre className="bg-gray-100 p-2 rounded overflow-x-auto">
+        {/* <code>{snippet.code}</code> */}
+      </pre>
+    </div>
+  );
+})
+  return (
+
+   <div>
+
+{renderedsnippets}
+
+
+   </div>
   );
 }
